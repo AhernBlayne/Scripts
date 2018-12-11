3a231d685db9098834428c63ab3fe5e3cc11bc13
AhernBlayne <45766482+AhernBlayne@users.noreply.github.com>
2018-12-11 20:47:13 +0000

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] Sounds;
+
+	// Use this for initialization
+	void Awake () {
+		foreach (Sound s in Sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+
+		}
+		
+	}
+	void Start ()
+	{
+		Play("cardriving");
+		
+	}
+	
+	public void Play (string name)
+	{
+		Sound s = Array.Find(Sounds,Sound => Sound.name == name);
+		s.source.Play();
+	}
+}
